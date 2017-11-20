43acb48475b9d3e4c0b2f3622cce7578b7039b1a
Tyler Matthews <trm84@zips.uakron.edu>
2017-11-20 17:59:01 -0500

    Fixed Uart Comm. Uart send and receive works now.

--- testCode/main.c
+++ testCode/main.c
@@ -32,13 +32,19 @@ void uart_init();
 void oscSetup();
 void T1_Setup();
 void UARTSend (char data);
+void __attribute__((interrupt, auto_psv)) _U1RXInterrupt(void);
+void __attribute__((interrupt, auto_psv)) _T1Interrupt(void);
+
 
 int booleans = 0;
 volatile int counter = 0;
-
+volatile int incomingData = 1;
+volatile int testData = 0;
+//0 = output, 1 = input
 void main(void) {
+   // TX2_Tris = 1;
+   // RX2_Tris = 0;
     char temp = 1;
-    RX2_Tris = 0;
     TRISBbits.TRISB9 = 0;
     LATBbits.LATB9 = 1;
     oscSetup();
@@ -49,9 +55,15 @@ void main(void) {
     
     while (1){
         if (counter == 50){
-            printf("Nick is a bitch \n");
-
+            //printf("%i",testData);
             //UARTSend(temp);
+            if (incomingData){
+                printf("testing\n");
+
+            }else{
+                printf("paused");
+            }
+
             temp = temp + 1;
             counter = 0;
             
@@ -72,19 +84,41 @@ void main(void) {
 //UxBRG=[Fcy/(16*Baud Rate)]-1.
 void uart_init(){
 
-    RPINR18 = 0x07;  //Make Pin RP7 U1RX
-    RPOR3bits.RP7R = 0x03;    // Make Pin RP8 U1TX
-    U1MODEbits.STSEL = 0; // 1-stop bit
-    U1MODEbits.PDSEL = 0; // No Parity, 8-data bits
-    U1MODEbits.ABAUD = 0; // Auto-Baud Disabled
-    U1MODEbits.BRGH = 0; // Low Speed mode
+    RPINR18bits.U1RXR = 0x08;  //Make Pin RP8 U1RX
+    RPOR3bits.RP7R = 0x03;    // Make Pin RP7 U1TX
+    
     U1BRG = 155; // BAUD Rate Setting
-    U1STAbits.UTXISEL1 = 0;
-    IEC0bits.U1TXIE = 0; // Disenable UART Tx interrupt
+
+    /* Does not fix issue
+    IPC2bits.U1RXIP = 111;
+    IPC0bits.T1IP = 110;
+    IPC3bits.U1TXIP = 101;
+    */  
+    U1STAbits.URXISEL = 0; // Interrupt after one RX character is received; 
+
+    IEC0bits.U1RXIE = 1; //Enable RX interrupt
+    IFS0bits.U1RXIF = 0; // Clear RX Interrupt flag  
+
     U1MODEbits.UARTEN = 1; // Enable UART
     U1STAbits.UTXEN = 1; // Enable UART Tx
+  
+
+  
 }
 
+void __attribute__((interrupt, auto_psv)) _U1RXInterrupt(void){
+    testData = U1RXREG; //This is how to get the data from the buffer
+    //Currently just checking if the interrupt is called by pausing the uart TX
+    IFS0bits.U1RXIF = 0; // Clear RX Interrupt flag   
+    
+    
+    if (incomingData == 1){
+        incomingData = 0;
+    }else{
+        incomingData = 1;
+    }
+
+} 
 
 void UARTSend (char data){
     U1TXREG = data;
@@ -128,7 +162,6 @@ void T1_Setup(void){
 }
 
 void __attribute__((interrupt, auto_psv)) _T1Interrupt(void){
-    
     counter = counter +1 ;
     IFS0bits.T1IF = 0;
 }
